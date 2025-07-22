fa29555b3b56b44b49f99169b309fb5fe6c2aab4
clara-zhang-5 <55606172+clara-zhang-5@users.noreply.github.com>
2025-07-22 13:56:43 -0700

    finalized footer styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,38 +1,47 @@
 import '../styles/Footer.css';
 import '../styles/fonts.css';
 import { Link } from 'react-router-dom';
+import linkedinIcon from '../assets/linkedin.svg';
+import instagramIcon from '../assets/instagram.svg';
+import behanceIcon from '../assets/behance.svg';
+import usFlag from '../assets/usa.png';
+import indiaFlag from '../assets/india.png';
 
 function Footer() {
     return (
         <div className="footer">
             <footer>
-                <div className="footer-content">
-                    <div>
-                        <div className="content-wrapper">
-                            <p className="h2">
+                <div className="footer-content content-wrapper">
+                    <div className="footer-main-content">
+                        <p className="h2">
                             Ready to Elevate Your Brand With Customized Digital Solutions?
-                            </p>
-                            <p className="body-large">
-                                Meraaki Designs is a global creative ops studio delivering digital execution, 
-                                automation strategy, and interface design in collaboration with trusted tech partners.
-                            </p>
-                            <p className="body-small">Locations:</p>
-                            <div className="locations">
-                                <div className="body-extra-small">
-                                    <img src="" alt="flag" className="body-extra-small" />
-                                    <p className="body-extra-small">IND</p>
-                                </div>
-                                <div className="body-extra-small">
-                                    <img src="" alt="flag" className="body-extra-small" />
-                                    <p className="body-extra-small">USA</p>
-                                </div>
+                        </p>
+                        <p className="body-small">
+                            Meraaki Designs is a global creative ops studio delivering digital execution, 
+                            automation strategy, and interface design in collaboration with trusted tech partners.
+                        </p>
+                        <p className="body-small" style={{fontWeight:"bold"}}>Locations:</p>
+                        <div className="locations">
+                            <div className="location">
+                                <img src={indiaFlag} alt="flag" className="body-extra-small" style={{width:"30px"}}/>
+                                <p className="body-extra-small">IND</p>
+                            </div>
+                            <div className="location">
+                                <img src={usFlag} alt="flag" className="body-extra-small" style={{width:"30px"}}/>
+                                <p className="body-extra-small">USA</p>
                             </div>
                         </div>
                     </div>
-                    <div className="content-wrapper footer-social-links">
-                        <button>LinkedIn</button>
-                        <button>Instagram</button>
-                        <button>be</button>
+                    <div className="footer-social-links">
+                        <button>
+                            <img src={linkedinIcon} alt="li" />
+                        </button>
+                        <button>
+                            <img src={instagramIcon} alt="ig" />
+                        </button>
+                        <button>
+                            <img src={behanceIcon} alt="be" />
+                        </button>
                     </div>
                 </div>
                 <ul className="footer-links" style={{textDecoration:"none"}}>
